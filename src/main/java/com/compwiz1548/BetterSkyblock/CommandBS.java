bbfbb8f8031ed3a0f2bcb8f7a3134c0180cb8a0b
compwiz1548 <compwiz1548@gmail.com>
2016-04-18 00:04:01 -0400

    1.0

---
+++ src/main/java/com/compwiz1548/BetterSkyblock/CommandBS.java
@@ -0,0 +1,102 @@
+package com.compwiz1548.BetterSkyblock;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static com.compwiz1548.BetterSkyblock.Main.*;
+
+public class CommandBS implements CommandExecutor
+{
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args)
+    {
+        if (args.length > 0)
+        {
+            if (commandSender instanceof Player)
+            {
+                if (args[0].equalsIgnoreCase("create"))
+                    if (getUUIDs().contains(((Player) commandSender).getUniqueId()))
+                        commandSender.sendMessage("Island already created! Use /bs home to go to your island!");
+                    else
+                    {
+                        boolean op = commandSender.isOp();
+                        commandSender.setOp(true);
+                        Bukkit.dispatchCommand(commandSender, "island create " + ((Player) commandSender).getDisplayName().toLowerCase());
+                        plugin.addUUID(((Player) commandSender).getUniqueId());
+                        Bukkit.dispatchCommand(commandSender, "island join " + ((Player) commandSender).getDisplayName().toLowerCase());
+                        commandSender.setOp(op);
+                    }
+                else if (args[0].equalsIgnoreCase("visit"))
+                {
+                    if (args.length > 1)
+                    {
+                        if (!getUUIDs().contains(((Player) commandSender).getUniqueId()))
+                            commandSender.sendMessage("Island doesn't exist!");
+                        else
+                        {
+                            boolean op = commandSender.isOp();
+                            commandSender.setOp(true);
+                            Bukkit.dispatchCommand(commandSender, "island join " + args[1].toLowerCase());
+                            commandSender.setOp(op);
+                            commandSender.sendMessage("Teleported to " + args[1].toLowerCase());
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (args[0].equalsIgnoreCase("home"))
+                {
+                    if (getUUIDs().indexOf(((Player) commandSender).getUniqueId()) == -1)
+                        commandSender.sendMessage("Island doesn't exist! Use /bs create to create your island!");
+                    else
+                    {
+                        boolean op = commandSender.isOp();
+                        commandSender.setOp(true);
+                        Bukkit.dispatchCommand(commandSender, "island join " + ((Player) commandSender).getDisplayName().toLowerCase());
+                        commandSender.setOp(op);
+                    }
+                }
+                else if (args[0].equalsIgnoreCase("setspawn"))
+                {
+                    if (args.length == 1)
+                    {
+//                        Location loc = ((Player) commandSender).getLocation();
+//                        boolean op = commandSender.isOp();
+//                        commandSender.setOp(true);
+//                        Bukkit.dispatchCommand(commandSender, "island setspawn " + ((Player) commandSender).getDisplayName() + " " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
+//                        commandSender.setOp(op);
+//                        commandSender.sendMessage("Spawn set to X:" + loc.getX() + " Y:" + loc.getY() + " Z:" + loc.getZ());
+                        commandSender.sendMessage("Not implemented yet!");
+                    }
+                    else if (args.length == 4)
+                    {
+//                        boolean op = commandSender.isOp();
+//                        commandSender.setOp(true);
+//                        Bukkit.dispatchCommand(commandSender, "island setspawn " + ((Player) commandSender).getDisplayName() + " " + args[1] + " " + args[2] + " " + args[3]);
+//                        commandSender.setOp(op);
+//                        commandSender.sendMessage("Spawn set to X:" + args[1] + " Y:" + args[2] + " Z:" + args[3]);
+                        commandSender.sendMessage("Not implemented yet!");
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/compwiz1548/BetterSkyblock/Main.java
@@ -0,0 +1,97 @@
+package com.compwiz1548.BetterSkyblock;
+
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Main extends JavaPlugin
+{
+
+    public static volatile Main plugin = null;
+    public static Server server;
+    public static ArrayList<UUID> uuids = new ArrayList<UUID>();
+    public File uuidFile;
+
+    public static ArrayList<UUID> getUUIDs()
+    {
+        return uuids;
+    }
+
+    @Override
+    public void onEnable()
+    {
+        if (plugin == null)
+            plugin = this;
+        server = getServer();
+
+        plugin.getCommand("bs").setExecutor(new CommandBS());
+
+        if (!getDataFolder().exists())
+        {
+            getDataFolder().mkdirs();
+        }
+        uuidFile = new File(getDataFolder(), "uuids.txt");
+        createFile();
+        loadUUIDs(uuidFile);
+    }
+
+    @Override
+    public void onDisable()
+    {
+        //Nothing to do
+    }
+
+    private void createFile()
+    {
+        try
+        {
+            if (!uuidFile.exists())
+            {
+                uuidFile.createNewFile();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadUUIDs(File uuidFile)
+    {
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader(uuidFile));
+
+            String line = in.readLine();
+            while (line != null)
+            {
+                uuids.add(UUID.fromString(line));
+                line = in.readLine();
+            }
+            in.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void addUUID(UUID uuid)
+    {
+        uuids.add(uuid);
+        try
+        {
+            BufferedWriter out = new BufferedWriter(new FileWriter(uuidFile, true));
+
+            out.append(uuid.toString() + "\n");
+            out.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
